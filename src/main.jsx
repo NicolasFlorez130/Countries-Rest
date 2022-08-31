e07b1ba45784e4f54898adfc72c6c2fe8f9fe5de
Nicolas <nmflorezr@gmail.com>
2022-08-31 15:22:22 -0500

    First setting

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import GlobalStyles from './assets/GlobalStyles';
+import './assets/index.scss'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+   <React.StrictMode>
+      <GlobalStyles />
+      <App />
+   </React.StrictMode>
+);
