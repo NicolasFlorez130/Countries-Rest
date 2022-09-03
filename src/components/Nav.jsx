dfd182811e39856e118928a5ce501f1c08829b9b
Nicolas <nmflorezr@gmail.com>
2022-09-03 16:10:21 -0500

    Persistence in the theme and favicons added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
 
+if (localStorage.getItem('theme') == null) {
+   localStorage.setItem('theme', ' Dark Mode');
+}
+
 const Navigation = styled.nav`
    ${tw`
       bg-skin-element
@@ -23,14 +27,23 @@ const Navigation = styled.nav`
 `;
 
 const Nav = ({ wrapper }) => {
-   const [activeTheme, setActiveTheme] = useState(' Dark Mode');
+   const [activeTheme, setActiveTheme] = useState(localStorage.getItem('theme'));
    const [iconClasses, setIconClasses] = useState('fa-solid fa-moon');
+
+   useEffect(() => {
+      if (localStorage.getItem('theme') == ' Light Mode') {
+         wrapper.current.classList.add('light');
+         setIconClasses('fa-solid fa-sun');
+      }
+   }, []);
 
    const changeTheme = () => {
       if (wrapper.current.classList.toggle('light')) {
+         localStorage.setItem('theme', ' Light Mode');
          setActiveTheme(' Light Mode');
          setIconClasses('fa-solid fa-sun');
       } else {
+         localStorage.setItem('theme', ' Dark Mode');
          setActiveTheme(' Dark Mode');
          setIconClasses('fa-solid fa-moon');
       }
